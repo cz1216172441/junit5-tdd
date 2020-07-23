442e188a829f45bb005375145c46f387877b342e
CHENRO6 <chenro6@oocl.com>
2020-07-23 11:49:56 +0800

    [junit5-tdd][Roy] feature: pass should_return_buzz_when_count_off_given_5

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -6,6 +6,9 @@ public class FizzBuzz {
         if (inputNumber % 3 == 0) {
             return "Fizz";
         }
+        if (inputNumber % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(inputNumber);
     }
 
